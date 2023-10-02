412d06467de9d77073d84e421e0c9fa9953969d6
Denys <dkononov.ua@gmail.com>
2023-10-02 14:50:56 +0300

    fix

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,9 +1,15 @@
 /* eslint-disable prettier/prettier */
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import fs from 'fs'
+
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const httpsOptions = {
+    key: fs.readFileSync('./etc/letsencrypt/live/sky.syrykh.com:8000/privkey.pem'),
+    cert: fs.readFileSync('./etc/letsencrypt/live/sky.syrykh.com:8000/cert.pem'),
+  };
+  const app = await NestFactory.create(AppModule, {httpsOptions});
   app.enableCors({
     origin: '*',
     methods: ['GET', 'POST'],
